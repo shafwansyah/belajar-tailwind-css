f3b6773184aac03f9a959e6a0d3cae90ce9763cd
shafwansyah <shafwansyah250@gmail.com>
2022-04-23 01:54:18 +0700

    dark mode 1/2

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,22 +1,43 @@
-// hamburger
-
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
-
-hamburger.addEventListener('click', ()=>{
-    hamburger.classList.toggle('hamburger-active')
-    navMenu.classList.toggle('hidden')
-})
-
 // navbar fixed
+window.onscroll = () => {
+  const header = document.querySelector("header");
+  const toTop = document.querySelector("#toTop");
+  const fixedNav = header.offsetTop;
 
-window.onscroll = ()=>{
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
 
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed')
-    } else {
-        header.classList.remove('navbar-fixed')
-    }
-}
+// hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// klik diluar hamburger
+window.addEventListener("click", (e) => {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", () => {
+  darkToggle.checked
+    ? html.classList.add("dark")
+    : html.classList.remove("dark");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-  content: ['./public/**/*.{html, js}'],
+  content: ["./public/**/*.{html, js}", "./*.{html,js}"],
+  darkMode: "class",
   theme: {
-    container:{
+    container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#14b8a6',
-        dark : '#0f172a',
-        secondary : '#64748b'
+        primary: "#14b8a6",
+        dark: "#0f172a",
+        secondary: "#64748b",
       },
       screens: {
-        '2xl' :'1320px',
-      }
+        "2xl": "1320px",
+      },
     },
   },
   plugins: [],
-}
+};
